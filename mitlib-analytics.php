906f1b094c8eb1d9a175ab3b9a4c496a51b36436
Matt Bernhardt <mjbernha@mit.edu>
2017-06-08 16:33:10 -0400

    Converts properties from get_option to get_site_option; closes NTI-99
    This also builds a distinct form handler, because apparently Wordpress can't
    do that automatically (or at least the process isn't documented).

--- mitlib-analytics.php
+++ mitlib-analytics.php
@@ -3,7 +3,7 @@
  * Plugin Name: MITlib Analytics
  * Plugin URI: https://github.com/MITLibraries/mitlib-analytics
  * Description: This plugin provides a thin implementation of Google Analytics tracking for use across domains.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Matt Bernhardt for MIT Libraries
  * Author URI: https://github.com/MITLibraries
  * License: GPL2
@@ -138,7 +138,7 @@ function mitlib_analytics_section_mit() {
  */
 function mitlib_analytics_property_callback() {
 	// Get the settings value.
-	$options = get_option( 'mitlib_ga_property' );
+	$options = get_site_option( 'mitlib_ga_property' );
 	?>
 	<input
 		type="text"
@@ -155,7 +155,7 @@ function mitlib_analytics_property_callback() {
  */
 function mitlib_analytics_domain_callback() {
 	// Get the settings value.
-	$options = get_option( 'mitlib_ga_domains' );
+	$options = get_site_option( 'mitlib_ga_domains' );
 	?>
 	<input
 		type="text"
@@ -181,7 +181,7 @@ function mitlib_analytics_domain_callback() {
  */
 function mitlib_analytics_mit_property_callback() {
 	// Get the settings value.
-	$options = get_option( 'mitlib_mit_property' );
+	$options = get_site_option( 'mitlib_mit_property' );
 	?>
 	<input
 		type="text"
@@ -202,12 +202,51 @@ function mitlib_analytics_page_html() {
 		return;
 	}
 
+	// Maybe handle post actions here?
+	$action = filter_input( INPUT_POST, 'action' );
+	if ( ! empty( $action ) ) {
+		// Check for validity.
+		echo '<div class="updated"><p>Analytics settings updated.</p></div>';
+		check_admin_referer( 'custom_nonce_action', 'custom_nonce_field' );
+
+		// Update settings.
+		if ( 'update' == filter_input( INPUT_POST, 'action' ) ) {
+			// Set default values.
+			$mitlib_ga_property = '';
+			$mitlib_ga_domains = '';
+			$mitlib_mit_property = '';
+			// GA Property.
+			if ( filter_input( INPUT_POST, 'mitlib_ga_property' ) ) {
+				$mitlib_ga_property = sanitize_text_field(
+					wp_unslash( filter_input( INPUT_POST, 'mitlib_ga_property' ) )
+				);
+			}
+			// GA domain list.
+			if ( filter_input( INPUT_POST, 'mitlib_ga_domains' ) ) {
+				$mitlib_ga_domains = sanitize_text_field(
+					wp_unslash( filter_input( INPUT_POST, 'mitlib_ga_domains' ) )
+				);
+			}
+			// MIT Property.
+			if ( filter_input( INPUT_POST, 'mitlib_mit_property' ) ) {
+				$mitlib_mit_property = sanitize_text_field(
+					wp_unslash( filter_input( INPUT_POST, 'mitlib_mit_property' ) )
+				);
+			}
+			update_site_option( 'mitlib_ga_property', $mitlib_ga_property );
+			update_site_option( 'mitlib_ga_domains', $mitlib_ga_domains );
+			update_site_option( 'mitlib_mit_property', $mitlib_mit_property );
+		}
+	}
+
 	// Build the form.
 	?>
 	<div class="wrap">
 		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-		<form method="post" action="../options.php">
+		<form method="post" action="admin.php?page=mitlib-analytics">
 			<?php
+			// Nonce field, because Wordpress can't manage to do this itself.
+			wp_nonce_field( 'custom_nonce_action', 'custom_nonce_field' );
 			// Output security fields for this form.
 			settings_fields( 'mitlib_analytics' );
 			// Output settings sections and their fields.
@@ -224,14 +263,14 @@ function mitlib_analytics_page_html() {
  * View function that outputs the GA code on public pages.
  */
 function mitlib_analytics_view() {
-	$domains = explode( ',', get_option( 'mitlib_ga_domains' ) );
-	$mit = get_option( 'mitlib_mit_property' );
+	$domains = explode( ',', get_site_option( 'mitlib_ga_domains' ) );
+	$mit = get_site_option( 'mitlib_mit_property' );
 	echo "<script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', '" . esc_html( get_option( 'mitlib_ga_property' ) ) . "',  'auto', {'allowLinker': true});
+	ga('create', '" . esc_html( get_site_option( 'mitlib_ga_property' ) ) . "',  'auto', {'allowLinker': true});
 	ga('require', 'linker');
 	ga('linker:autoLink', [";
 	foreach ( $domains as &$item ) {
